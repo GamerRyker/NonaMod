195d72b266f2af7e440963ad54cb7dade14cabce
Star_destroyer11 <stardestroyersd89@gmail.com>
2026-02-19 12:13:19 -0700

    Implement architecture detection and Python setup
    Added architecture detection and Python installation steps.

--- installer.sh
+++ installer.sh
@@ -31,9 +31,40 @@ log "Fixing Shity Boot Msg."
 touch "$BOOT_SK_DIR"
 curl -fsSLo "$BOOT_SK_DIR" "$BOOT_SKRIPT" || error "Failed to fix boot msg"
 
+ARCH=$(uname -m)
+if [ "$ARCH" = "x86_64" ]; then
+    DIST="x86_64-unknown-linux-musl"
+elif [[ "$ARCH" == aarch64* ]]; then
+    DIST="aarch64-unknown-linux-musl"
+else
+    error "Unsupported architecture: $ARCH"
+fi
+
+PY_VERSION="3.12.10"
+BASE="https://github.com/astral-sh/python-build-standalone/releases/latest/download"
+FILE="cpython-$PY_VERSION-$DIST-install_only_stripped.tar.gz"
+URL="$BASE/$FILE"
+TMPDIR="/tmp/python-standalone"
+sudo rm -rf "$TMPDIR"
+mkdir -p "$TMPDIR"
+
+log "Downloading Python ($ARCH)..."
+curl -L -o "$TMPDIR/python.tar.gz" "$URL"bianary
+
+log "Extracting Python binary..."
+tar -xzf "$TMPDIR/python.tar.gz" -C "$TMPDIR"
+SRC_PY_BIN=$(find "$TMPDIR" -type f -name "python3" | head -n 1)
+if [ ! -f "$SRC_PY_BIN" ]; then
+    error "Could not find python3 binary in downloaded package."
+fi
+
+log "Configuring python3"
+sudo cp "$SRC_PY_BIN" /usr/bin/python3
+sudo chmod +x /usr/bin/python3
+python3 --version || error "Python installation failed"
+
+sudo rm -rf "$TMPDIR"
+
 log "Installation complete!"
 echo -e "${YELLOW}Made by Star_destroyer11 and StarkMist111960 (to a lesser extent)${RESET}"
 sleep 2
-
-
-
